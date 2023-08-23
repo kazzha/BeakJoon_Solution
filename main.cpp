d56316201028acb59083f00d6012bb2a8ce6b642
kazz <gang9696@gmail.com>
2023-08-23 14:57:55 +0900

    2480

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 int main()
 {
-	int year{};
-	std::cin >> year;
-	
-	if ((year % 4) == 0) {
-		if (year % 100 != 0 || year % 400 == 0) {
-			std::cout << 1;
-		}
-		else { std::cout << 0; }
-	}
-	else { std::cout << 0; }
-}
+	int h{}, m{}, timer{};
 
+	cin >> h >> m;
+	cin >> timer;
+
+	h += (m + timer) / 60;
+
+	if (h > 23)
+	{
+		cout << h - 24 << " " << (m + timer) % 60;
+	}
+	else {
+		cout << h << " " << (m + timer) % 60;
+	}
+}
