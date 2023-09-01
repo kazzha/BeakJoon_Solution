a4c92f0453489f245c524936496cfb7a4a47d136
kazz <gang9696@gmail.com>
2023-09-01 22:18:02 +0900

    벌집..

--- main.cpp
+++ main.cpp
@@ -2,28 +2,19 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
+
 using namespace std;
 
-
+ 
 int main()
 {
-	string subject, score;
-	double Time{}, change{}, totalScore{}, timeScore{};
+	int N{}, oneWall{1}, dotCount{4};
 
-	map<string, double> ScoreChange{ {"A+", 4.5},{"A0",4.0},{"B+",3.5},{"B0",3.0},{"C+",2.5},
-		{"C0",2.0},{"D+",1.5},{"D0",1.0},{"F",0.0},{"P",0.0} };
+	cin >> N;
 	
-	for (int i = 0; i < 20; i++)
-	{
-		cin >> subject >> Time >> score;
-		change = ScoreChange[score];
-		if (score != "P")
-		{
-			totalScore += (change * Time);
-			timeScore += Time;
-		}
-    }
-	cout.precision(7);
-	cout << totalScore / timeScore;
+	oneWall = oneWall + pow(2,N);
+    dotCount = oneWall * oneWall;
+	
+
+	cout << dotCount;
 }
