b9b8bdfae3c97cb9e3b7d8abb0e4948fa5b42447
kazz <gang9696@gmail.com>
2023-09-08 19:57:58 +0900

    big o solve

--- main.cpp
+++ main.cpp
@@ -5,34 +5,18 @@
 
 using namespace std;
 
- 
+bool f(int a1, int a0, int c, int n0)
+{
+	
+    if (c-a1 <0 || (a1 * n0) + a0 > c * n0) { return false; }
+	
+	return true;
+}
+
 int main()
 {
-	vector<int> div;
-	int N{}, sum{};
+	int a1{}, a0{}, c{}, n0{};
 
-	cin >> N;
-
-	while (N != -1) {
-		for (int i = 1; i < N; i++)
-		{
-			if ((N % i) == 0) { div.push_back(i); sum += i; }
-
-		}
-		if (sum != N) { cout << N << " is NOT perfect." << endl; }
-		else if (sum == N) {
-			cout << N << " = ";
-			for (int i = 0; i < div.size(); i++)
-			{
-				cout << div[i];
-				if (i != (div.size() - 1)) cout << " + ";
-			}
-			cout << endl;
-		}
-
-		div.clear();
-		sum = 0;
-		cin >> N;
-	}
-	
+	cin >> a1 >> a0 >> c >> n0;
+	cout << f(a1, a0, c, n0);
 }
