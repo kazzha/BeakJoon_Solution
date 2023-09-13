ae2f46d57e6d9ebdf642b1a437d7c6cf13ea44f4
kazz <gang9696@gmail.com>
2023-09-13 18:02:51 +0900

    brute

--- main.cpp
+++ main.cpp
@@ -7,20 +7,4 @@ using namespace std;
 
 int main()
 {
-	int n{}, num{ 666 };
-	string tmp;
-
-	cin >> n;
-
-	while (n > 0)
-	{
-		tmp = to_string(num);
-		if (tmp.find("666") != string::npos)
-		{
-			n--;
-		}
-		num++;
-	}
-
-	cout << tmp;
 }
--- 브루트포스_영화감독 숌.h
+++ 브루트포스_영화감독 숌.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n{}, num{ 666 };
+	string tmp;
+
+	cin >> n;
+
+	while (n > 0)
+	{
+		tmp = to_string(num);
+		if (tmp.find("666") != string::npos)
+		{
+			n--;
+		}
+		num++;
+	}
+
+	cout << tmp;
+}
