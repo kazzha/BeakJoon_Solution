decaf82185a98e7e1fef21d37dd16af68e8645df
kazz <gang9696@gmail.com>
2023-10-01 23:45:11 +0900

    brute force

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+/*
 bool CompareI(const pair<int, string>& a, const pair<int, string>& b)
 {
 
@@ -43,5 +44,43 @@ int main()
     }
 
     이걸 카운팅 정렬로 바꾸기
+
+}
+
+*/
+
+int main()
+{
+    int N{}, M{};
+
+    cin >> N;
+
+    vector<int> sangun(N);
+    for (int i = 0; i < N; i++)
+    {
+        cin >> sangun[i];
+    }
+
+    cin >> M;
+    vector<int> num(M);
+    vector<bool> checking(M);
+
+    for (int j = 0; j < M; j++)
+    {
+        cin >> num[j];
+        for (int k = 0; k < N; k++)
+        {
+            if (num[j] == sangun[k])
+            {
+                checking[j] = true;
+            }
+        }
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        cout << checking[i] << " ";
+    }
+
 
 }
