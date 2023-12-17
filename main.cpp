97d9a7c84f22485f0a542bad2bee18908aa2fa11
kazz <gang9696@gmail.com>
2023-12-17 23:36:24 +0900

    minus accumulate solve

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
@@ -9,46 +10,36 @@ int main()
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
     
-    int N{}, M{}, start{};
-    cin >> N >> M;
-    vector<vector<int>> numbers(N, vector<int>(N));
-    vector<vector<int>> sums(N, vector<int>(N));
+    string sik;
+    cin >> sik;
 
-    for (int i = 0; i < N; i++)
+    istringstream iss(sik);
+    int num{}, sum{};
+    char op{'+'};
+    bool minus = false;
+
+    iss >> num;
+    sum += num;
+
+    while (iss >> op >> num)
     {
-        for (int j = 0; j < N; j++)
+        if(op == '-')
         {
-            cin >> numbers[i][j];
-            if (j == 0) {
-                sums[i][j] = numbers[i][j];
-            }
-            else
-            {
-                sums[i][j] = sums[i][j - 1] + numbers[i][j];
-            }
-        }
-    }
-    
-    int x1{}, x2{}, y1{}, y2{}, ans{};
-
-    for (int i = 0; i < M; i++)
-    {
-        cin >> x1 >> y1 >> x2 >> y2;
-
-        for (int j = x1; j <= x2; j++)
-        {
-            if (y1 - 2 >= 0)
-                ans += sums[j - 1][y2 - 1] - sums[j - 1][y1 - 2];
-
-            else
-                ans += sums[j - 1][y2 - 1];
+            minus = true;
         }
 
-        cout << ans << '\n';
-        ans = 0;
+        if(op == '-' || minus)
+        {
+            sum -= num;
+        }
+        else
+        {
+            sum += num;
+        }
     }
 
-    
+    cout << sum;
+
 }
 
 
